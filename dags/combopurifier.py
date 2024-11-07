74ebc7a9783a829f44ac5b01760f1fad533b4d79
Leonardo Pinho <leonardo@cloudificando.com>
2024-11-06 22:49:11 -0300

    some changes

--- dags/combopurifier.py
+++ dags/combopurifier.py
@@ -39,6 +39,7 @@ TEMPLATE_PATH = "/opt/airflow/dags/repo/spark-jobs/combopurifier/combopurifier_s
         'fromjson': lambda s: json.loads(s),
         'tojson': lambda x: json.dumps(x)
     },
+    render_template_as_native_obj=True
 )
 def init():
     @task
